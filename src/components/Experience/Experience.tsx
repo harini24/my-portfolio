bfdcaee65ac225c530115bdacb0865a67cd03eb1
harini24 <harinisiva1997@gmail.com>
2022-11-02 00:06:00 +0530

    changes to make experience responsive

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -1,27 +1,43 @@
 import { Box, styled } from "@mui/material";
-import {
-  ReactElement,
-  JSXElementConstructor,
-  ReactFragment,
-  ReactPortal,
-} from "react";
+import LinkIcon from "@mui/icons-material/Link";
 import Securin from "../../assets/securin.jpg";
 import Verizon from "../../assets/verizon.jpg";
 interface Experience {
   span?: string;
   company?: string;
+  location?: string;
   progress?: any;
 }
 interface Prop {
   experience: Experience;
 }
 
-const Wrapper = styled(Box)(() => ({
+const Wrapper = styled(Box)(({ theme }) => ({
   display: "flex",
   alignItems: "center",
   padding: "0 0 20px",
+  "& .span": {
+    padding: "0 0 6px",
+  },
+  "& .description": {
+    padding: "0 0 6px 12px",
+    display: "flex",
+    alignItems: "center",
+    "& .bullet": {
+      height: 10,
+      minWidth: 10,
+      backgroundColor: "black",
+      borderRadius: "50%",
+      marginRight: 10,
+    },
+  },
+
+  [theme.breakpoints.down("md")]: {
+    flexDirection: "column",
+    alignItems: "flex-start",
+  },
 }));
-const CompanyWrapper = styled(Box)(() => ({
+const CompanyWrapper = styled(Box)(({ theme }) => ({
   display: "flex",
   "& .logo": {
     marginRight: 10,
@@ -38,6 +54,11 @@ const CompanyWrapper = styled(Box)(() => ({
     flexDirection: "column",
     alignItems: "center",
   },
+  [theme.breakpoints.down("md")]: {
+    "& .employment-info": {
+      alignItems: "flex-start",
+    },
+  },
 }));
 const DescriptionWrapper = styled(Box)(() => ({
   width: 450,
@@ -51,7 +72,7 @@ const logo: Map = {
 };
 
 const Experience = ({ experience }: Prop) => {
-  const { company = "", span, progress = [] } = experience;
+  const { company = "", span, location, progress = [] } = experience;
   return (
     <Wrapper>
       <CompanyWrapper>
@@ -59,27 +80,28 @@ const Experience = ({ experience }: Prop) => {
           <img src={logo[company.toLowerCase()]} alt="logo" />
         </div>
         <div className="employment-info">
-          {progress?.length === 1 && <div>{progress[0].role}</div>}
           <div>{company}</div>
+          <div>{location}</div>
           <div>{span}</div>
         </div>
       </CompanyWrapper>
       <DescriptionWrapper>
-        {progress.length === 1 &&
-          progress[0].descriptions.map((desc: string) => <div>{desc}</div>)}
-        {progress.length > 1 &&
-          progress.map((prog: any) => {
-            return (
-              <>
-                <div>{prog.role}</div>
-                <div>
-                  {prog.descriptions.map((desc: string) => (
-                    <div>{desc}</div>
-                  ))}
-                </div>
-              </>
-            );
-          })}
+        {progress.map((prog: any) => {
+          return (
+            <>
+              <div className="role">{prog.role}</div>
+              <div className="span">{prog.span}</div>
+              <div>
+                {prog.descriptions.map((desc: string) => (
+                  <div className="description">
+                    <div className="bullet" />
+                    {desc}
+                  </div>
+                ))}
+              </div>
+            </>
+          );
+        })}
       </DescriptionWrapper>
     </Wrapper>
   );
